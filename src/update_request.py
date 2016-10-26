f73ea3a39650d9d2acfc2464a0e4f52381b8cb80
ArjanSchouten <arjan-schouten@live.nl>
2016-10-26 17:54:02 +0200

    Fix bugs

--- src/update_request.py
+++ src/update_request.py
@@ -10,14 +10,14 @@ class UpdateRequest(Resource):
     def get(self, version):
         response = flask.make_response()
         response.headers['Content-Type'] = 'application/octet-stream'
-        file = 'firmwareupdate - ' + version + '.bin'
+        file = 'firmwareupdate-' + version + '.bin'
         response.headers['Content-Disposition'] = 'attachment; filename=\'+file'+'\''
         if os.path.isfile(self.storage + file) is False:
             abort(404)
 
         f = open('./firmwareupdate-' + version + '.bin', 'r')
         hash_md5 = hashlib.md5()
-        response.data = f.xreadlines()
+        response.data = f.read()
         hash_md5.update(response.data)
         response.headers['x-MD5'] = hash_md5.hexdigest()
         f.close()
