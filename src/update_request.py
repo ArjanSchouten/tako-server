e202bdaf2154ec5e944010227887a82f902ce7cb
ArjanSchouten <arjan-schouten@live.nl>
2016-10-26 17:36:06 +0200

    updated firmware updates

--- src/update_request.py
+++ src/update_request.py
@@ -1,13 +1,24 @@
+import hashlib
+import os
+
 import flask
-from flask_restful import Resource
+from flask_restful import Resource, abort
 
 
 class UpdateRequest(Resource):
+    storage = '/var/www/firmwarestore/'
     def get(self, version):
         response = flask.make_response()
         response.headers['Content-Type'] = 'application/octet-stream'
-        #response.headers['x-MD5'] = 'this is a hash'
+        file = 'firmwareupdate - ' + version + '.bin'
+        response.headers['Content-Disposition'] = 'attachment; filename=\'+file'+'\''
+        if os.path.isfile(self.storage + file) is False:
+            abort(404)
+
         f = open('./firmwareupdate-' + version + '.bin', 'r')
-        response.data = f.readlines()
+        hash_md5 = hashlib.md5()
+        response.data = f.xreadlines()
+        hash_md5.update(response.data)
+        response.headers['x-MD5'] = hash_md5.hexdigest()
         f.close()
         return response
